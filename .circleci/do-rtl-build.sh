8079deb1f8b66bfb9d43e948edc1c13cffcd9de8
abejgonzalez <abe.j.gonza@gmail.com>
2019-07-06 17:23:10 -0700

    update

--- .circleci/do-rtl-build.sh
+++ .circleci/do-rtl-build.sh
@@ -10,17 +10,17 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 source $SCRIPT_DIR/server.sh
 
 JOB_DIR=$WORK_DIR/chipyard-$1
-SIM_DIR=$JOB_DIR/chipyard/sims/verisim
+SIM_DIR=$JOB_DIR/sims/verisim
 
 # set stricthostkeychecking to no (must happen before rsync)
 run "echo \"Ping $SERVER\""
 
 run "mkdir -p $JOB_DIR"
-copy /home/riscvuser/chipyard $SERVER:$JOB_DIR
+run "cp -R $WORK_DIR/chipyard/ $JOB_DIR"
 
 # enter the verisim directory and build the specific config on remote server
 run "make -C $SIM_DIR clean"
 run "make -C $SIM_DIR RISCV=$WORK_DIR/riscv-tools-install VERILATOR_INSTALL_DIR=$WORK_DIR/verilator JAVA_ARGS=\"-Xmx8G -Xss8M\" SUB_PROJECT=boom CONFIG=$1 TOP=BoomRocketSystem"
 
 # copy back the final build
-copy $SERVER:$JOB_DIR/chipyard /home/riscvuser/chipyard
+copy $SERVER:$JOB_DIR/ /home/riscvuser/chipyard
