5426b70d25d64c95c9865775396b4f198a6d60d2
Abraham Gonzalez <abe.j.gonza@gmail.com>
2019-10-28 21:59:13 -0700

    remove build store for now

--- .circleci/defaults.sh
+++ .circleci/defaults.sh
@@ -81,5 +81,12 @@ LOCAL_CHIPYARD_DIR=$HOME/chipyard
 LOCAL_VERILATOR_DIR=$HOME/verilator
 LOCAL_SIM_DIR=$LOCAL_CHIPYARD_DIR/sims/verilator
 
-# obtain the key value store for the builds
-./build-store.sh
+# key value store to get the build strings
+declare -A mapping
+mapping["smallboom"]="CONFIG=SmallBoomConfig"
+mapping["mediumboom"]="CONFIG=MediumBoomConfig"
+mapping["largeboom"]="CONFIG=LargeBoomConfig"
+mapping["megaboom"]="CONFIG=MegaBoomConfig"
+mapping["boomandrocket"]="CONFIG=SmallBoomAndRocketConfig"
+mapping["rv32boom"]="CONFIG=SmallRV32BoomConfig"
+mapping["hwachaboom"]="CONFIG=HwachaLargeBoomConfig"
