ad851fa5b7c3f00370b33f997450f3dc3b15afc2
Abraham Gonzalez <abe.j.gonza@gmail.com>
2019-10-28 22:11:57 -0700

    try to put all in script

--- .circleci/firesim-init-manager.sh
+++ .circleci/firesim-init-manager.sh
@@ -12,21 +12,30 @@ source $SCRIPT_DIR/defaults.sh
 # TODO: CLEAR OLD FIRESIM DIR
 #run_script $LOCAL_CHECKOUT_DIR/.circleci/clean-old-files.sh $CI_DIR
 
-run_aws "git clone --progress --verbose https://github.com/ucb-bar/chipyard.git $REMOTE_AWS_CHIPYARD_DIR \
-         cd $REMOTE_AWS_CHIPYARD_DIR \
-         echo \"Checking out Chipyard version: $(cat $LOCAL_CHECKOUT_DIR/CHIPYARD.hash)\" \
-         git fetch \
-         git checkout $(cat $LOCAL_CHECKOUT_DIR/CHIPYARD.hash) \
-         ./scripts/init-submodules-no-riscv-tools.sh \
-         ./scripts/firesim-setup.sh --fast \
-         firesim managerinit <<EOD \
-         \
-         \
-         \
-         \
-         EOD \
-         rm -rf $REMOTE_CHIPYARD_DIR/generators/boom"
+# create a script to run
+cat <<EOF >> firesim-manager-setup.sh
+#!/bin/bash
+
+cd $REMOTE_AWS_WORK_DIR
+git clone --progress --verbose https://github.com/ucb-bar/chipyard.git $REMOTE_AWS_CHIPYARD_DIR
+cd $REMOTE_AWS_CHIPYARD_DIR
+echo \"Checking out Chipyard version: $(cat $LOCAL_CHECKOUT_DIR/CHIPYARD.hash)\"
+git fetch
+git checkout $(cat $LOCAL_CHECKOUT_DIR/CHIPYARD.hash)
+./scripts/init-submodules-no-riscv-tools.sh
+./scripts/firesim-setup.sh --fast
+firesim managerinit <<EOD
+
+
+
+
+EOD 
+rm -rf $REMOTE_AWS_CHIPYARD_DIR/generators/boom
+EOF
 
 #         git checkout -C $REMOTE_AWS_MARSHAL_DIR MY_HASH_WITH_THE_CAT_PROC_STUFF"
+
+chmod +x firesim-manager-setup.sh
+run_script_aws firesim-manager-script.sh
 
 copy $LOCAL_CHECKOUT_DIR $AWS_SERVER:$REMOTE_AWS_CHIPYARD_DIR/generators/boom/
