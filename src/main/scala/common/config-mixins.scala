4ae309bc2d6bf028ac7f98a336dece435c40799d
Jerry Zhao <jerryz123@berkeley.edu>
2021-03-10 22:53:44 -0800

    Add TAGE bpd to cs152

--- src/main/scala/common/config-mixins.scala
+++ src/main/scala/common/config-mixins.scala
@@ -454,3 +454,31 @@ class WithSWBPD extends Config((site, here, up) => {
     ))
   }
 })
+
+class WithTAGELBPD extends Config((site, here, up) => {
+  case BoomTilesKey => up(BoomTilesKey, site) map { b =>
+    b.copy(core = b.core.copy(
+      bpdMaxMetaLength = 120,
+      globalHistoryLength = 64,
+      localHistoryLength = 1,
+      localHistoryNSets = 0,
+      branchPredictor = ((resp_in: BranchPredictionBankResponse, p: Parameters) => {
+        val loop = Module(new LoopBranchPredictorBank()(p))
+        val tage = Module(new TageBranchPredictorBank()(p))
+        val btb = Module(new BTBBranchPredictorBank()(p))
+        val bim = Module(new BIMBranchPredictorBank()(p))
+        val ubtb = Module(new FAMicroBTBBranchPredictorBank()(p))
+        val preds = Seq(loop, tage, btb, ubtb, bim)
+        preds.map(_.io := DontCare)
+
+        ubtb.io.resp_in(0)  := resp_in
+        bim.io.resp_in(0)   := ubtb.io.resp
+        btb.io.resp_in(0)   := bim.io.resp
+        tage.io.resp_in(0)  := btb.io.resp
+        loop.io.resp_in(0)  := tage.io.resp
+
+        (preds, loop.io.resp)
+      })
+    ))
+  }
+})
